761de888bad66cb0a3ed7ecc0e3daef33a2934f5
AATMANI Ousama <106839668+AatmaniOusama@users.noreply.github.com>
2023-11-25 22:47:25 +0100

    final-commit
    swagger integration

---
+++ .history/src/main/java/com/ous/bio/ws/security/AuthenticationFilter_20231120181426.java
@@ -0,0 +1,83 @@
+package com.ous.bio.ws.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ous.bio.ws.SpringApplicationContext;
+import com.ous.bio.ws.requests.UserLoginRequest;
+import com.ous.bio.ws.services.UserService;
+import com.ous.bio.ws.shared.dto.UserDto;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+            throws AuthenticationException {
+        try {
+
+            UserLoginRequest creds = new ObjectMapper().readValue(req.getInputStream(), UserLoginRequest.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+        try {
+            String userName = ((User) auth.getPrincipal()).getUsername();
+
+            UserService userService = (UserService) SpringApplicationContext.getBean("userServiceImpl");
+
+            UserDto userDto = userService.getUser(userName);
+
+            if (userDto != null) {
+                String token = Jwts.builder()
+                        .setSubject(userName)
+                        .claim("id", userDto.getUserId())
+                        .claim("name", userDto.getFirstName() + " " + userDto.getLastName())
+                        .setIssuedAt(new Date())
+                        .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                        .signWith(SignatureAlgorithm.HS384, SecurityConstants.TOKEN_SECRET.getBytes())
+                        .compact();
+
+                res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+                res.addHeader("user_id", userDto.getUserId());
+
+                res.getWriter().write("{\"token\": \"" + token + "\", \"id\": \"" + userDto.getUserId() + "\"}");
+            } else {
+                // Handle the case where the userDto is null
+                res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User not found");
+            }
+        } catch (Exception e) {
+            // Log the exception or handle it accordingly
+            res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error generating JWT: " + e.getMessage());
+        }
+}
+}
